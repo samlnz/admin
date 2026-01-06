8d1d4a4fea58d2c6ab4c4c85564b1f59846f7332
samlnz <82331715+samlnz@users.noreply.github.com>
2026-01-07 00:19:19 +0300

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    # In production, use Gunicorn instead of app.run
-    app.run(host='0.0.0.0', port=5000, debug=False)
